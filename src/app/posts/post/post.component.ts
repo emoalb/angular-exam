ffaf7d108665881b207f33f3d971721d41db6ea5
emoalb <e_alb@abv.bg>
2019-04-23 14:49:14 +0300

    Some bugs removed

---
+++ src/app/posts/post/post.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {PostModel} from "../models/post.model";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+@Input() post: PostModel;
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+
+  }
+
+}
+
