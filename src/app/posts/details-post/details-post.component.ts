5f47a112746662f90291b7c42600857532ff86be
emoalb <e_alb@abv.bg>
2019-04-24 11:58:58 +0300

    Admin can delete and edit all posts

--- src/app/posts/details-post/details-post.component.ts
+++ src/app/posts/details-post/details-post.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {PostService} from "../services/post.service";
 import {PostModel} from "../models/post.model";
-import { Location } from '@angular/common';
+import {Location} from '@angular/common';
 import {AuthService} from "../../authentication/auth.service";
 
 @Component({
@@ -14,31 +14,32 @@ export class DetailsPostComponent implements OnInit {
 
 
   constructor(private route: ActivatedRoute
-              , private postService: PostService
-              , private location: Location,  private router: Router, private authService:AuthService) {
+    , private postService: PostService
+    , private location: Location, private router: Router, private authService: AuthService) {
   }
 
   post: PostModel;
 
   ngOnInit() {
     const _id: string = this.route.snapshot.paramMap.get('id');
-    this.postService.getPostById(_id).subscribe((post: PostModel)=>{
-      this.post=post;
+    this.postService.getPostById(_id).subscribe((post: PostModel) => {
+      this.post = post;
     })
   }
+
   goBack(): void {
     this.location.back();
   }
 
-  isCreator():boolean {
-    return this.post.username===localStorage.getItem('username');
+  isCreator(): boolean {
+    return this.post.username === localStorage.getItem('username');
   }
 
   deletePost() {
-this.postService.deletePost(this.post._id).subscribe();
+    this.postService.deletePost(this.post._id).subscribe();
   }
 
   editPost() {
-    this.router.navigate(['edit/'+this.post._id]);
+    this.router.navigate(['edit/' + this.post._id]);
   }
 }
